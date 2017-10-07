a2497c83f1c62116988d8424c07d3aaf8eaa0eac
Jonathan M Nguyen <31453488+jonathanMNg@users.noreply.github.com>
2017-10-07 01:17:36 -1000

    Task 5
    Task 5: tasklist subscriptions

--- app/tasklist.js
+++ app/tasklist.js
@@ -1,6 +1,7 @@
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
+  Meteor.subscribe('tasks');
   Template.tasks.helpers({
     tasks: function() {
       return Tasks.find({}, {sort: {createdAt: -1}});
@@ -28,7 +29,9 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-
+  Meteor.publish('tasks', function() {
+    return Tasks.find({userId: this.userId});
+  });
 }
 
 Meteor.methods({
@@ -39,7 +42,7 @@ Meteor.methods({
     Tasks.insert({
       name: name,
       createdAt: new Date(),
-      userId: Meteor.userId
+      userId: Meteor.userId()
     });
   },
   deleteTask : function(taskId) {
